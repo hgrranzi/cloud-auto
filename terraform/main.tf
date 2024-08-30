6eceb536c925bfadf0682008fcc4260697eb073e
hgrranzi <vkh.mariia@gmail.com>
2024-08-30 15:52:34 +0200

    set security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,11 @@ variable "prefix" {
   default     = "dev"
 }
 
+variable admin_access_cidr_blocks {
+  type = list(string)
+  default = []
+}
+
 resource "aws_vpc" "app-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -60,4 +65,54 @@ resource "aws_internet_gateway" "app-internet-gateway" {
 resource "aws_route_table_association" "app-route_table_a" {
   route_table_id = aws_route_table.app-route-table.id
   subnet_id      = aws_subnet.app-subnet-1.id
+}
+
+resource "aws_security_group" "app-security-group" {
+  name   = "app-security-group"
+  vpc_id = aws_vpc.app-vpc.id
+
+  # todo: no need ssh access at all probably
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.admin_access_cidr_blocks
+  }
+
+  ingress {
+    description = "PHPMyAdmin access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = var.admin_access_cidr_blocks
+  }
+
+  ingress {
+    description = "HTTP WordPress access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS WordPress access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Outbound traffic access"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name : "${var.prefix}-security-group"
+  }
 }
